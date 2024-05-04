2b7da704e1d97431e93cdc31a697f3b978f4503e
Puchkoved <168586094+Puchkoved@users.noreply.github.com>
2024-05-04 14:32:42 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,27 @@
+my_list =['apple', 'banana', 'orange', 'kiwi']
+print (my_list)
+print (my_list[0],my_list[-1])
+print (my_list[2:4])
+my_list[2]= 'watermelon'
+print (my_list)
+my_dict = {'apple': 'яблоко', 'banana': 'банан', 'orange': 'апельсин'}
+print (my_dict)
+print (my_dict['apple'])
+my_dict['watermelon']='арбуз'
+print (my_dict)
+my_dict['watermelon']='водяная дыня'
+print (my_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
