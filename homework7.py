641f498845996393b63e17ae6b23878cf3a5c7f1
pawlo <pawlowdanil@yandex.ru>
2024-05-05 13:04:19 +0300

    Условный оператор. Как работает оператор if

--- homework7.py
+++ homework7.py
@@ -1,1 +1,7 @@
-print()
+car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for i in range(len(car)):
+    print('Я езжу на автомабиле марки',car[i])
+    if i > 0:
+        cars_count += 10
+        print (cars_count)
