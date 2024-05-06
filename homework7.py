274c604d54eabdca2d1902dd4dff79b5fdd5be4f
pawlo <pawlowdanil@yandex.ru>
2024-05-06 12:41:13 +0300

    Цикл for. Элементы списка. Полезные функции в цикле

--- homework7.py
+++ homework7.py
@@ -2,6 +2,5 @@ car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
 cars_count = 0
 for i in range(len(car)):
     print('Я езжу на автомабиле марки',car[i])
-    if i > 0:
-        cars_count += 10
-        print (cars_count)
+    cars_count += 10
+    print (cars_count)
