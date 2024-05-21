d04bde8132568c700d5ed57a4d1f1495aeb82c53
pawlo <pawlowdanil@yandex.ru>
2024-05-21 21:13:30 +0300

    Домашняя работа по уроку "Специальные методы классов"

---
+++ homework14.py
@@ -0,0 +1,20 @@
+class house:
+
+    def __init__(self):
+        self.numberOfFloors = 0
+
+
+
+    def setNewNumberOfFloors (self,floors):
+        self.numberOfFloors = floors
+        print (self.numberOfFloors)
+
+
+h1 = house()
+house.setNewNumberOfFloors(h1,10)
+
+
+
+
+
+
