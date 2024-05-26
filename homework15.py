af05bb3511452d7153a3547e9950eb76399a2c71
pawlo <pawlowdanil@yandex.ru>
2024-05-26 11:56:25 +0300

    Перегрузка операторов.

--- homework15.py
+++ homework15.py
@@ -8,17 +8,17 @@ class Building:
 
 
     def __eq__(self, other):
-        return self.numberOfFloors == other.numberOfFlors and self.buildingType == other.buildingType
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
 
 
-b1 = (10, 'кирпич')
-b2 = (10, 'кирпич')
+b1 = Building(10, 'кирпич')
+b2 = Building(10, 'кирпич')
 print(b1 == b2)
-b1 = (10, 'дерево')
-b2 = (10, 'кирпич')
+b1 = Building(10, 'дерево')
+b2 = Building(10, 'кирпич')
 print(b1 == b2)
-b1 = (15, 'дерево')
-b2 = (10, 'кирпич')
+b1 = Building(15, 'кирпич')
+b2 = Building(10, 'кирпич')
 print(b1 == b2)
 
 
