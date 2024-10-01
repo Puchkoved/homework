623805156240e153596f73353ae14e29e2ae7cc6
pawlo <pawlowdanil@yandex.ru>
2024-10-01 16:57:56 +0300

    Исправил User на Users

--- homework34.py
+++ homework34.py
@@ -3,10 +3,10 @@ import sqlite3
 connection = sqlite3.connect('not_telegram.db')
 cursor = connection.cursor()
 
-cursor.execute('DELETE FROM User WHERE username = ? ', ('User6',))
-cursor.execute('SELECT COUNT(*) FROM User')
+cursor.execute('DELETE FROM Users WHERE username = ? ', ('User6',))
+cursor.execute('SELECT COUNT(*) FROM Users')
 total_users = cursor.fetchone()[0]
-cursor.execute('SELECT SUM(balance) FROM User')
+cursor.execute('SELECT SUM(balance) FROM Users')
 all_balances = cursor.fetchone()[0]
 print(all_balances/total_users)
 
