e11bbeb0ef45b70e21b78f9f3514a926935706db
pawlo <pawlowdanil@yandex.ru>
2024-10-01 16:53:50 +0300

    Выбор элементов и функции в SQL запросах. Исправил User на Users

--- homework33.py
+++ homework33.py
@@ -4,7 +4,7 @@ connection = sqlite3.connect('not_telegram.db')
 cursor = connection.cursor()
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS User(
+CREATE TABLE IF NOT EXISTS Users(
 ID INTEGER PRIMARY KEY,
 username TEXT NOT NULL,
 email TEXT NOT NULL,
@@ -14,11 +14,11 @@ balance INTEGER NOT NULL
 ''')
 
 for i in range(1, 11):
-    cursor.execute('INSERT INTO User (username, email, age, balance) VALUES(?,?,?,?)',
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES(?,?,?,?)',
                    (f'User{i}', f'example{i}@gmail.com', f'{10 * i}', '1000'))
 for i in range(1, 11, 2):
-    cursor.execute('UPDATE User SET balance = ? WHERE username = ? ', (500, f'User{i}'))
+    cursor.execute('UPDATE Users SET balance = ? WHERE username = ? ', (500, f'User{i}'))
 for i in range(1, 11, 3):
-    cursor.execute('DELETE FROM User WHERE username = ? ', (f'User{i}',))
+    cursor.execute('DELETE FROM Users WHERE username = ? ', (f'User{i}',))
 connection.commit()
 connection.close()
